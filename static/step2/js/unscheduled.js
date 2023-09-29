d9a24b38b7547295d62cd4024b00077cc055eb43
rohit <tomarrohit5034@gmail,com>
2023-09-29 13:50:02 +0530

    made changes

--- static/step2/js/unscheduled.js
+++ static/step2/js/unscheduled.js
@@ -9,9 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const cookie = cookies[i].trim();
         // Does this cookie string begin with the name we want?
         if (cookie.substring(0, name.length + 1) === name + "=") {
-          cookieValue = decodeURIComponent(
-            cookie.substring(name.length + 1)
-          );
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
           break;
         }
       }
@@ -107,7 +105,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-
   const itemsPerPage = 5;
   let currentPage = 1;
 
@@ -134,12 +131,12 @@ document.addEventListener("DOMContentLoaded", () => {
       return data;
     } catch (error) {
       alert("Error fetching data:");
-      return { data: []};
+      return { data: [] };
     }
   };
   // fetchedSocials()
 
-   function displayData(showData, totalCount) {
+  function displayData(showData, totalCount) {
     let container = document.getElementById("article-list");
     container.innerHTML = "";
 
@@ -166,22 +163,21 @@ document.addEventListener("DOMContentLoaded", () => {
         }
       }
       const handleSubmit = async () => {
-        const data = await fetchedSocials()
+        const data = await fetchedSocials();
+        console.log("[Linked account]", data);
         const checkboxes = form.querySelectorAll("input[type='checkbox']");
         let selectedArticle = { ...showData[index] };
         selectedArticle.social = [];
         selectedArticle.special = [];
-        
+
         checkboxes.forEach((checkbox) => {
           if (checkbox.checked) {
-           if (!data.includes(checkbox.value)) {
-              console.log(checkbox.value);
-              
+            if (!data.includes(checkbox.value)) {
               //Custom Notification popup
               function displayNotification(message) {
                 // Create a new notification element
-                var notification = document.createElement('div');
-                notification.classList.add('custom-notification');
+                var notification = document.createElement("div");
+                notification.classList.add("custom-notification");
                 notification.innerHTML = `
                 <div class="notification">
                   ${message}
@@ -192,47 +188,53 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.body.appendChild(notification);
 
                 // Add an event listener to the close button
-                var closeButton = notification.querySelector('.close-button');
-                closeButton.addEventListener('click', function () {
-                    notification.style.display = 'none';
+                var closeButton = notification.querySelector(".close-button");
+                closeButton.addEventListener("click", function () {
+                  notification.style.display = "none";
                 });
 
                 // Automatically close the notification after a set duration
-                notification.classList.add('timeout');
-                var contentElement = notification.querySelector('.notification');
+                notification.classList.add("timeout");
+                var contentElement =
+                  notification.querySelector(".notification");
                 var animationDuration = 10000; // Animation duration in milliseconds
                 var animationStartTime = Date.now();
 
                 function decreaseWidth() {
-                    var currentTime = Date.now();
-                    var elapsedTime = currentTime - animationStartTime;
-                    var progress = elapsedTime / animationDuration;
-                    var updatedWidth = 100 - (progress * 100); // Decrease width linearly over time
+                  var currentTime = Date.now();
+                  var elapsedTime = currentTime - animationStartTime;
+                  var progress = elapsedTime / animationDuration;
+                  var updatedWidth = 100 - progress * 100; // Decrease width linearly over time
 
-                    contentElement.style.setProperty('--after-width', updatedWidth + '%');
+                  contentElement.style.setProperty(
+                    "--after-width",
+                    updatedWidth + "%"
+                  );
 
-                    if (progress < 1) {
-                        requestAnimationFrame(decreaseWidth);
-                    } else {
-                        notification.style.display = 'none'; // Hide the notification
-                    }
+                  if (progress < 1) {
+                    requestAnimationFrame(decreaseWidth);
+                  } else {
+                    notification.style.display = "none"; // Hide the notification
+                  }
                 }
 
                 requestAnimationFrame(decreaseWidth);
               }
 
-
-              // You can use like so: 
-              displayNotification(`${checkbox.value} account have not been linked`);
-           }else{
-            console.log(checkbox.value);
-              if (checkbox.value === "twitter" || checkbox.value === "pinterest") {
+              // You can use like so:
+              displayNotification(
+                `${checkbox.value} account have not been linked`
+              );
+            } else {
+              if (
+                checkbox.value === "twitter" ||
+                checkbox.value === "pinterest"
+              ) {
                 selectedArticle.special.push(checkbox.value);
               } else {
                 selectedArticle.social.push(checkbox.value);
               }
-
-           }
+            }
           }
         });
 
@@ -242,9 +244,7 @@ document.addEventListener("DOMContentLoaded", () => {
           ).value;
           selectedArticle.schedule = dateFormat(scheduleValue);
         }
-
-        console.log(selectedArticle, index);
-        // sendData(selectedArticle, options);
+        sendData(selectedArticle, options);
 
         // Remove the dateTimeEl element from the form if it was added during the process
         if (dateTimeEl.parentElement === scheduleBtns) {
@@ -280,93 +280,90 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
-
   function displayPagination(totalCount) {
     const totalPages = Math.ceil(totalCount / itemsPerPage);
 
-    const paginationContainer = document.getElementById(
-      "paginationContainer"
-    );
+    const paginationContainer = document.getElementById("paginationContainer");
     paginationContainer.innerHTML = "";
 
-      // Previous Button
-      const prevButton = document.createElement('button');
-      prevButton.textContent = 'Previous';
-      prevButton.addEventListener('click', () => {
-        if (currentPage > 1) {
-          currentPage--;
-          displayDataForCurrentPage();
-          updatePaginationButtons();
-        }
-      });
-
-      paginationContainer.appendChild(prevButton);
-
-
-      // Display page numbers
-
-      const startPage = (Math.floor((currentPage - 1) / 5) * 5) + 1;
-      const endPage = Math.min(startPage + 4, totalPages);
-
-
-      //Current page number
-      for (let i = startPage; i <= endPage; i++) {
-        const pageLink = document.createElement('a');
-        pageLink.textContent = i;
-        pageLink.href = '#';
-        pageLink.addEventListener('click', (event) => {
-          event.preventDefault();
-          currentPage = i;
-          displayDataForCurrentPage();
-          updatePaginationButtons();
-        });
-        paginationContainer.appendChild(pageLink);
+    // Previous Button
+    const prevButton = document.createElement("button");
+    prevButton.textContent = "Previous";
+    prevButton.addEventListener("click", () => {
+      if (currentPage > 1) {
+        currentPage--;
+        displayDataForCurrentPage();
+        updatePaginationButtons();
       }
+    });
 
+    paginationContainer.appendChild(prevButton);
 
-      // More Button
-      const moreButton = document.createElement('span');
-      if (totalPages<= 7) {
-        moreButton.style.display = "none"
-      }
-      moreButton.textContent = '>>';
-      moreButton.addEventListener('click', () => {
-        const maxPage = Math.ceil(totalPages / 7); // Increase '5' to display more page numbers at once
-        const nextGroupLastPage = currentPage + 5;
-        currentPage = Math.min(nextGroupLastPage, maxPage);
+    // Display page numbers
+
+    const startPage = Math.floor((currentPage - 1) / 5) * 5 + 1;
+    const endPage = Math.min(startPage + 4, totalPages);
+
+    //Current page number
+    for (let i = startPage; i <= endPage; i++) {
+      const pageLink = document.createElement("a");
+      pageLink.textContent = i;
+      pageLink.href = "#";
+      pageLink.addEventListener("click", (event) => {
+        event.preventDefault();
+        currentPage = i;
         displayDataForCurrentPage();
         updatePaginationButtons();
       });
-      paginationContainer.appendChild(moreButton);
+      paginationContainer.appendChild(pageLink);
+    }
 
+    // More Button
+    const moreButton = document.createElement("span");
+    if (totalPages <= 7) {
+      moreButton.style.display = "none";
+    }
+    moreButton.textContent = ">>";
+    moreButton.addEventListener("click", () => {
+      const maxPage = Math.ceil(totalPages / 7); // Increase '5' to display more page numbers at once
+      const nextGroupLastPage = currentPage + 5;
+      currentPage = Math.min(nextGroupLastPage, maxPage);
+      displayDataForCurrentPage();
+      updatePaginationButtons();
+    });
+    paginationContainer.appendChild(moreButton);
 
-      // Next Button
-      const nextButton = document.createElement('button');
-      nextButton.textContent = 'Next';
-      nextButton.addEventListener('click', () => {
-        if (currentPage < totalPages) {
-          currentPage++;
-          displayDataForCurrentPage();
-          updatePaginationButtons();
-        }
-      });
+    // Next Button
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "Next";
+    nextButton.addEventListener("click", () => {
+      if (currentPage < totalPages) {
+        currentPage++;
+        displayDataForCurrentPage();
+        updatePaginationButtons();
+      }
+    });
 
-      paginationContainer.appendChild(nextButton);
+    paginationContainer.appendChild(nextButton);
 
-      updatePaginationButtons(totalCount);
+    updatePaginationButtons(totalCount);
   }
 
   function updatePaginationButtons(totalCount) {
     const totalPages = Math.ceil(totalCount / itemsPerPage);
 
-    const paginationLinks = document.querySelectorAll('#paginationContainer a');
-    const prevButton = document.querySelector('#paginationContainer button:nth-child(1)');
-    const nextButton = document.querySelector('#paginationContainer button:nth-last-child(1)');
+    const paginationLinks = document.querySelectorAll("#paginationContainer a");
+    const prevButton = document.querySelector(
+      "#paginationContainer button:nth-child(1)"
+    );
+    const nextButton = document.querySelector(
+      "#paginationContainer button:nth-last-child(1)"
+    );
 
     paginationLinks.forEach((link) => {
-      link.classList.remove('active');
+      link.classList.remove("active");
       if (Number(link.textContent) === currentPage) {
-        link.classList.add('active');
+        link.classList.add("active");
       }
     });
 
@@ -375,19 +372,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   async function displayDataForCurrentPage() {
-        $("html, body").animate({ scrollTop: 0 });
+    $("html, body").animate({ scrollTop: 0 });
     const { data, totalCount } = await fetchedData();
 
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const dataToShow = data.slice(startIndex, endIndex);
-    console.log(dataToShow);
-    displayData( dataToShow, totalCount);
+    displayData(dataToShow, totalCount);
     displayPagination(totalCount);
   }
 
-
   displayDataForCurrentPage();
-
-
 });
