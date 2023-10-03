7e3080d86d01281a7ec8e6bd4645afebc4318c80
Noble <nobleuwuja@gmail.com>
2023-10-03 11:16:05 +0100

    updated setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "./components/navbar/Navbar"
+import Step1 from "./pages/step1/Step1"
+import Sidebar from "./components/sidebar/Sidebar"
+import Step2 from "./pages/step2/Step2"
+
 function App() {
 
   return (
     <>
      <div>
-      Hello World
+      <div className="grid ">
+        <Navbar/>
+        <div className="grid grid-cols-6">
+          <div className="col-span-1">
+
+          <Sidebar/>
+          </div>
+
+        <Routes>
+          <Route path='/' element={<Step1/>}/>
+          <Route path='/step2' element={<Step2/>}/>
+        </Routes>
+        </div>
+
+      </div>
      </div>
     </>
   )
