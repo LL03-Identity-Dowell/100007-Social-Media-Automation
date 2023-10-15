6fd57a1f574222d6f342982a1f6fe998260dd595
Omair Waleed <owaleed@synapse-analytics.io>
2023-10-15 16:04:40 +0300

    completed adding specific post and specific article page

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -6,12 +6,18 @@ import step3 from "./images/Step-3.png";
 import step4 from "./images/Step-4.png";
 import step5 from "./images/Step-5.png";
 import LadyPixel from "./images/Lady-Pixel.png";
-
 import pens from "./images/pens.jpg";
-
+import teacherImg from "./images/teacher image.png";
 
 export {
-    logoImage, profile, step1, step2, step3, step4, step5, LadyPixel,
-    pens
-}
-
+  logoImage,
+  profile,
+  step1,
+  step2,
+  step3,
+  step4,
+  step5,
+  LadyPixel,
+  pens,
+  teacherImg,
+};
