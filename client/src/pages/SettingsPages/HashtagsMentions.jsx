29ec70ed1f89d8cb435a9838723375f30a86f9dc
Omair Waleed <owaleed@synapse-analytics.io>
2023-10-20 04:50:22 +0300

    finished modifyng ui and adding functionility of check box validation

--- client/src/pages/SettingsPages/HashtagsMentions.jsx
+++ client/src/pages/SettingsPages/HashtagsMentions.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { FaCheck } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 
@@ -10,6 +10,7 @@ const HashtagsMentions = ({ close }) => {
   const [inputMentionsText, setinputMentionsText] = useState("");
   const [inputMentionsList, setinputMentionsList] = useState([]);
   const [checkedMentionsList, setcheckedMentionsList] = useState([]);
+  const [isSaveDisabled, setIsSaveDisabled] = useState(true);
 
   const handleHashtagInputChange = (e) => {
     setinputHashtagText(e.target.value);
@@ -27,6 +28,7 @@ const HashtagsMentions = ({ close }) => {
       setcheckedHashtagList([...checkedHashtagList, false]);
     }
   };
+
   const handleAddMentionsInput = (e) => {
     e.preventDefault();
 
@@ -40,7 +42,6 @@ const HashtagsMentions = ({ close }) => {
   const handleAddInput = (e) => {
     handleAddHashtagInput(e);
     handleAddMentionsInput(e);
-
   };
 
   const handleRemoveHashtagInput = (index) => {
@@ -48,31 +49,64 @@ const HashtagsMentions = ({ close }) => {
     setcheckedHashtagList((prevChecked) => {
       const updatedChecked = [...prevChecked];
       updatedChecked.splice(index, 1);
+      updateSaveButtonState(updatedChecked, checkedMentionsList);
       return updatedChecked;
     });
+
   };
+
   const handleRemoveMentionsInput = (index) => {
     setinputMentionsList((prevList) => prevList.filter((_, i) => i !== index));
     setcheckedMentionsList((prevChecked) => {
       const updatedChecked = [...prevChecked];
       updatedChecked.splice(index, 1);
+      updateSaveButtonState(checkedHashtagList, updatedChecked);
       return updatedChecked;
     });
+
+
   };
 
   const handleCheckboxHashtagChange = (index) => {
-    setcheckedHashtagList((prevChecked) => {
-      const updatedChecked = [...prevChecked];
-      updatedChecked[index] = !updatedChecked[index];
-      return updatedChecked;
-    });
+    const updatedChecked = [...checkedHashtagList];
+    updatedChecked[index] = !updatedChecked[index];
+    setcheckedHashtagList(updatedChecked);
+
+    updateSaveButtonState(updatedChecked, checkedMentionsList);
   };
+
   const handleCheckboxMentionsChange = (index) => {
-    setcheckedMentionsList((prevChecked) => {
-      const updatedChecked = [...prevChecked];
-      updatedChecked[index] = !updatedChecked[index];
-      return updatedChecked;
-    });
+    const updatedChecked = [...checkedMentionsList];
+    updatedChecked[index] = !updatedChecked[index];
+    setcheckedMentionsList(updatedChecked);
+
+    updateSaveButtonState(checkedHashtagList, updatedChecked);
+  };
+
+  const updateSaveButtonState = (hashtagChecked, mentionsChecked) => {
+    const areAnyChecked = hashtagChecked.some((value) => value) || mentionsChecked.some((value) => value);
+    setIsSaveDisabled(!areAnyChecked);
+  };
+
+  const handleGetRequest = async () => {
+    try {
+      const response = await fetch(
+        `http://127.0.0.1:8000/api/v1/hash-tags-and-mentions/`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("can't make get request");
+      } else {
+        // request is ok
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
@@ -82,19 +116,19 @@ const HashtagsMentions = ({ close }) => {
   return (
     <div className="bg-pens bg-cover bg-center h-[90vh]">
       <div className="bg-overlay max-w-5xl mx-auto my-6 h-[85vh] shadow-lg shadow-gray-400">
-        <div className="flex justify-center items-center flex-col h-full w-full">
+        <div className="flex justify-center items-center flex-col h-full w-fill">
           <div>
-            <h2 className="text-customBlue font-bold text-2xl xl:text-3xl pb-12">
+            <h2 className="text-customBlue text-2xl xl:text-3xl pb-12">
               Hashtags and Mentions
             </h2>
           </div>
 
-          <form onSubmit={handleAddInput} className="w-full px-6">
+          <form onSubmit={handleAddInput} className="w-50 px-6">
             <div className="flex flex-col lg:flex-row gap-6 lg:gap-10 w-full">
               <div className="text-center w-full">
                 <label
                   htmlFor="hashtags"
-                  className="text-customBlue text-lg font-bold"
+                  className="text-customBlack text-lg "
                 >
                   Hashtags
                 </label>
@@ -110,24 +144,32 @@ const HashtagsMentions = ({ close }) => {
                     placeholder="Enter Hashtags.."
                     className="border-none bg-white w-full outline-2 outline-blue-400 "
                   />
-                  <button className="bg-customTextBlue text-white rounded-r-2xl px-2">
+                  <button
+                    onClick={handleAddHashtagInput}
+                    className="bg-blue-800 text-white rounded-r-2xl px-2"
+                  >
                     Add
                   </button>
                 </div>
-                <div >
-                  <ul>
+                <div>
+                  <ul className="flex flex-wrap">
                     {inputHashtagList.map((name, index) => (
-                      <li key={index}>
-                        <input
-                          type="checkbox"
-                          checked={checkedHashtagList[index]}
-                          onChange={() => handleCheckboxHashtagChange(index)}
-                        />{" "}
-                        {name}{" "}
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        <button onClick={() => handleRemoveHashtagInput(index)}>
-                          <FaTimes />
-                        </button>
+                      <li key={index} className="mb-4 mr-4">
+                        <div className="flex items-center">
+                          <input
+                            type="checkbox"
+                            checked={checkedHashtagList[index]}
+                            onChange={() => handleCheckboxHashtagChange(index)}
+                            className="mr-2"
+                          />
+                          {name}
+                          <button
+                            onClick={() => handleRemoveHashtagInput(index)}
+                            className="ml-8 text-gray-600"
+                          >
+                            <FaTimes />
+                          </button>
+                        </div>
                       </li>
                     ))}
                   </ul>
@@ -136,7 +178,7 @@ const HashtagsMentions = ({ close }) => {
               <div className="text-center w-full">
                 <label
                   htmlFor="mentions"
-                  className="text-customBlue text-lg font-bold"
+                  className="text-customBlack text-lg "
                 >
                   Mentions
                 </label>
@@ -152,34 +194,44 @@ const HashtagsMentions = ({ close }) => {
                     placeholder="Enter Mentions.."
                     className="border-none bg-white w-full"
                   />
-                  <button className="bg-customTextBlue text-white rounded-r-2xl px-2">
+                  <button
+                    onClick={handleAddMentionsInput}
+                    className="bg-blue-800 text-white rounded-r-2xl px-2"
+                  >
                     Add
                   </button>
                 </div>
                 <div>
-                  <ul>
+                  <ul className="flex flex-wrap">
                     {inputMentionsList.map((name, index) => (
                       <li key={index}>
                         <input
                           type="checkbox"
-                          checked={checkedMentionsList[index]} // Use the checked state
-                          onChange={() => handleCheckboxMentionsChange(index)} // Handle checkbox change
+                          checked={checkedMentionsList[index]}
+                          onChange={() => handleCheckboxMentionsChange(index)}
                         />{" "}
                         {name}{" "}
-                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <button
                           onClick={() => handleRemoveMentionsInput(index)}
+                          className="ml-8 text-gray-600"
                         >
                           <FaTimes />
                         </button>
+                        &nbsp;&nbsp;
                       </li>
                     ))}
                   </ul>
                 </div>
               </div>
             </div>
+
             <div className="flex justify-center items-center mt-6 md:mt-16">
-              <button className="bg-customTextBlue px-4 py-2 text-white rounded-md flex items-center gap-3">
+              <button
+                onClick={handleGetRequest}
+                className={`bg-blue-800 px-4 py-2 text-white rounded-md flex items-center gap-3 ${isSaveDisabled ? "bg-blue-300 cursor-not-allowed" : ""
+                  }`}
+                disabled={isSaveDisabled}
+              >
                 Save <FaCheck />
               </button>
             </div>
