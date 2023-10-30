a987152e3e1e297cb48a5e5e901a8d00acdb040e
Victor Yohanna <victoryohanna@gmail.com>
2023-10-30 10:01:13 +0100

    accept incoming changes and marged conflict

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-// import { store } from './app/store'
-import { Provider } from 'react-redux'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
->>>>>>> 7ebe829c3c85646ffbe05fe29e73101ef356740b
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
