55d0e27344fa3492e5e6c74ed25a60917b1407ff
Victor Yohanna <victoryohanna@gmail.com>
2023-11-02 10:35:49 +0100

    modified pagination component

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,62 +1,51 @@
-export const Pagination = ({ currentPage, setPageNumber, totalPage }) => {
-  const listOfNextPages =
-    currentPage === 1 || currentPage === 2
-      ? [1, 2, 3, 4, 5]
-      : [
-          currentPage - 2,
-          currentPage - 1,
-          currentPage,
-          currentPage + 1,
-          currentPage + 2,
-        ];
+import "bootstrap/dist/css/bootstrap.css";
 
-  const prevPage = () => {
-    if (currentPage > 1) {
-      setPageNumber((prev) => prev - 1);
-    }
-  };
+const Pagination = ({
+  pageCount,
+  totalPage,
+  currentPage,
+  prevPage,
+  nextPage,
+  paginate,
+}) => {
+  const pageNumber = [];
 
-  const nextPage = () => {
-    if (currentPage < totalPage) {
-      setPageNumber((prev) => prev + 1);
-    }
-  };
+  for (let i = 1; i <= Math.ceil(totalPage / pageCount); i++) {
+    pageNumber.push(i);
+  }
 
   return (
-    <div className='relative max-w-max'>
-      <button
-        className='hover:bg-[#333]/20 h-10 w-10 absolute left-[-60px] rounded-full '
-        onClick={prevPage}
-      >
-        Prev
-      </button>
-      <ul className='flex gap-x-8'>
-        {listOfNextPages.map((item) => {
-          if (item <= totalPage) {
-            return (
-              <li className='flex items-center justify-center ' key={item}>
-                <button
-                  onClick={() => setPageNumber(item)}
-                  className={`${
-                    currentPage === item && "bg-customBlue text-white"
-                  } rounded-full h-10 w-10  hover:bg-[#333]/20`}
-                >
-                  {item}
-                </button>
-              </li>
-            );
-          } else {
-            return null;
-          }
+    <nav>
+      <ul className="pagination">
+        <li className="page-item">
+          <a href="#" className="page-link" onClick={prevPage}>
+            Prev
+          </a>
+        </li>
+        {pageNumber.map((number, i) => {
+          return (
+            <li
+              className={`page-item ${currentPage === number ? "active" : ""}`}
+              key={i}
+            >
+              <a
+                href="#"
+                className="page-link"
+                onClick={() => paginate(number)}
+              >
+                {number}
+              </a>
+            </li>
+          );
         })}
-
-        <button
-          onClick={nextPage}
-          className='hover:bg-[#333]/20 rounded-full absolute right-[-60px] my-auto h-10 w-10'
-        >
-          Next
-        </button>
+        <li className="page-item">
+          <a href="#" className="page-link" onClick={nextPage}>
+            Next
+          </a>
+        </li>
       </ul>
-    </div>
+    </nav>
   );
 };
+
+export default Pagination;
