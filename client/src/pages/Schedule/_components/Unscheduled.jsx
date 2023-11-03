7b8883899b18fa36ca240341ad4930c88183aeab
Victor Yohanna <victoryohanna@gmail.com>
2023-11-03 11:43:33 +0100

    fix styling

--- client/src/pages/Schedule/_components/Unscheduled.jsx
+++ client/src/pages/Schedule/_components/Unscheduled.jsx
@@ -9,6 +9,7 @@ import {
 } from "./SocialComponent";
 
 import Pagination from "../../../components/Pagination";
+import ReactPaginate from "react-paginate";
 
 const UnscheduledPage = () => {
   const [unscheduledPost, setUnscheduledPost] = useState([]);
@@ -21,8 +22,9 @@ const UnscheduledPage = () => {
         withCredentials: true,
       })
       .then((response) => {
-        console.log(response.data)
+        console.log(response.data);
         let unscheduledData = response.data.Unscheduled_Posts;
+        console.log(unscheduledData);
         setUnscheduledPost(unscheduledData.response);
       })
       .catch((error) => {
@@ -54,10 +56,56 @@ const UnscheduledPage = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div className="px-20">
-      <h3 className="text-[#495057] font-bold">
-        Total posts count: {unscheduledPost.length}
-      </h3>
+    <div className="relative h-[100vh] max-w-7xl mx-auto lg:h-auto overflow-y-hidden lg:overflow-y-auto">
+      
+      <div className="count-article flex justify-between pt-0 pb-2 items-center">
+        <p className="px-6 py-3 italic">Total posts count: {unscheduledPost.length}</p>
+      </div>
+      {/* <div>
+        <div className="overflow-y-scroll lg:overflow-y-auto h-[70vh] lg:h-auto grid gap-6 lg:gap-10 pb-10">
+          <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+            <div className="articles">
+              {postData &&
+                postData.map((post, index) => (
+                  <div className="article mr-2 mt-6 " key={index}>
+                    
+                    <p className="lg:px-6 px-2 py-0 text-md lg:text-xl text-customTextBlue dark:text-white font-bold lg:w-[1000px]">
+                      {post.title}
+                    </p>
+
+                    <div className="content-button flex flex-col md:flex-row justify-between items-baseline py-0">
+                      <p className="lg:px-6 lg:pt-4 px-2 text-md lg:text-lg line-clamp-4 leading-loose lg:w-[1000px]">
+                        {post.paragraph}
+                      </p>
+
+                      <div className="lg:w-[150px] lg:pt-2 flex justify-end md:mr-6 mt-2 md:mt-0">
+
+                        <ExtraSmallBtn title={"View Post"} onClick={() => handlePostdetailNavigate(post.post_id, post.title, post.paragraph, post.source)} />
+
+                      </div>
+                    </div>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+
+        <ReactPaginate
+          pageCount={pageCount}
+          pageRangeDisplayed={pagesToDisplay}
+          marginPagesDisplayed={2}
+          onPageChange={handlePageClick}
+          previousLabel={<span className="text-black">Previous</span>}
+          nextLabel={<span className="text-black">Next</span>}
+          containerClassName="flex justify-center items-center my-4 space-x-2"
+          pageClassName="p-2 rounded-full cursor-pointer text-lg hover:bg-gray-300 w-[30px] h-[30px] md:w-[40px] md:h-[40px] flex justify-center items-center"
+          previousClassName="p-2 rounded-full cursor-pointer hover:bg-gray-300"
+          nextClassName="p-2 rounded-full cursor-pointer hover:bg-gray-300"
+          breakClassName="p-2"
+          activeClassName="bg-customBlue w-[30px] h-[30px] md:w-[40px] md:h-[40px] flex justify-center items-center text-white hover:bg-blue-600 "
+        />
+        
+      </div> */}
       <ul className="space-y-10 ">
         {currentPost.map((item) => (
           <li
@@ -70,7 +118,12 @@ const UnscheduledPage = () => {
               <h3 className="text-2xl font-bold text-customTextBlue">
                 {item.title}
               </h3>
-              <p className="text-[#333]">{item.paragraph}</p>
+              <div className="content-button flex flex-col md:flex-row justify-between items-baseline py-0">
+                <p className="lg:px-6 lg:pt-4 px-2 text-md lg:text-lg line-clamp-4 leading-loose lg:w-[1000px]">
+                  {item.paragraph}
+                </p>
+              </div>
+
               <div className="self-end space-x-8">
                 <Modal article={item} title="post">
                   <Dialog.Close asChild>
