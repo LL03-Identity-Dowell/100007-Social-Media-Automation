98e383d95f4c1eb56ff30b690b3bc47077d29cd9
Victor Yohanna <victoryohanna@gmail.com>
2023-11-15 08:54:26 +0100

    commit changes

--- client/src/pages/Schedule/_components/Unscheduled.jsx
+++ client/src/pages/Schedule/_components/Unscheduled.jsx
@@ -12,25 +12,39 @@ import ReactPaginate from "react-paginate";
 import Loading from "../../../components/Loading";
 import { ErrorMessages } from "../../../components/Messages";
 
-const UnscheduledPage = () => {  
-  
+const UnscheduledPage = () => {
+  const [count, setCount] = useState(0);
+  const [page, setPage] = useState(0);
+  const [perPage] = useState(5);
+  const [pageCount, setPageCount] = useState(0);
+  const [pagesToDisplay] = useState(7);
+  const [showMorePages, setShowMorePages] = useState(false);
   const [unscheduledPost, setUnscheduledPost] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  const url = "http://127.0.0.1:8000/api/v1/unscheduled-json/";
   const fetchUnscheduled = async () => {
     setIsError(null);
     setIsLoading(true);
     await axios
-      .get(url, {
-        withCredentials: true,
-      })
+      .get(
+        `http://127.0.0.1:8000/api/v1/unscheduled-json/?page=${
+          page + 1
+        }&order=newest`,
+        {
+          withCredentials: true,
+        }
+      )
       .then((response) => {
-        // console.log("response",response.data)
         setIsLoading(false);
-        let unscheduledData = response.data.Unscheduled_Posts;
-        setUnscheduledPost(unscheduledData.response);
+        let data = response.data.Unscheduled_Posts;
+        let totalItems = response.data.total_items;
+
+        setUnscheduledPost(data.response);
+        setCount(totalItems);
+        setPageCount(Math.ceil(totalItems / perPage));
+        setShowMorePages(pageCount > pagesToDisplay);
+        window.scrollTo(0, 0);
       })
       .catch((error) => {
         setIsLoading(false);
@@ -39,90 +53,101 @@ const UnscheduledPage = () => {
   };
   useEffect(() => {
     fetchUnscheduled();
-  }, []);
+  }, [page]);
 
-  const itemsPerPage = 5;
-  const [itemOffset, setItemOffset] = useState(0);
-  const endOffset = itemOffset + itemsPerPage;
-  const currentItems = unscheduledPost.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(unscheduledPost.length / itemsPerPage);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % unscheduledPost.length;
-    setItemOffset(newOffset);
+  const handlePageClick = (data) => {
+    setPage(data.selected);
   };
+
+  const loadMorePages = () => {
+    if (pageCount > pagesToDisplay) {
+      const nextPagesToDisplay = Math.min(pageCount - page, pagesToDisplay);
+      setShowMorePages(nextPagesToDisplay + page < pageCount);
+      setPage(page + nextPagesToDisplay);
+    }
+  };
+
+  // const itemsPerPage = 2;
+  // const [itemOffset, setItemOffset] = useState(0);
+  // const endOffset = itemOffset + itemsPerPage;
+  // const currentItems = unscheduledPost.slice(itemOffset, endOffset);
+  // const pageCount = Math.ceil(unscheduledPost.length / itemsPerPage);
+
+  // const handlePageClick = (event) => {
+  //   const newOffset = (event.selected * itemsPerPage) % unscheduledPost.length;
+  //   setItemOffset(newOffset);
+  // };
 
   return (
     <div className="relative h-[100vh] max-w-7xl mx-auto lg:h-auto overflow-y-hidden lg:overflow-y-auto">
       {isLoading && <Loading />}
       {isError && <ErrorMessages>{isError}</ErrorMessages>}
       <div className="count-article flex justify-between pt-0 pb-2 items-center">
-        <p className="px-6 py-3 italic">
-          Total posts count: {unscheduledPost.length}
-        </p>
+        <p className="px-6 py-3 italic">Total posts count: {count}</p>
       </div>
       <div>
         <div className="overflow-y-scroll lg:overflow-y-auto h-[70vh] lg:h-auto grid gap-6 lg:gap-10 pb-10">
           <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
             <div className="articles">
-              {currentItems.map((item, index) => (
-                <div className="article mr-2 mt-6 " key={index}>
-                  <p className="lg:px-6 px-2 py-0 text-md lg:text-xl text-customTextBlue dark:text-white font-bold lg:w-[1000px]">
-                    {item.title}
-                  </p>
-
-                  <div className="content-button flex flex-col md:flex-row justify-between items-baseline py-0">
-                    <p className="lg:px-6 lg:pt-4 px-2 text-md lg:text-lg line-clamp-4 leading-loose lg:w-[1000px]">
-                      {item.paragraph}
+              {unscheduledPost &&
+                unscheduledPost.map((item, index) => (
+                  <div className="article mr-2 mt-6 " key={index}>
+                    <p className="lg:px-6 px-2 py-0 text-md lg:text-xl text-customTextBlue dark:text-white font-bold lg:w-[1000px]">
+                      {item.title}
                     </p>
-                    <div className="self-end space-x-8">
-                      <img
-                        className="w-40 h-40  rounded-lg"
-                        src={item.image}
-                        alt="image"
-                      />
+
+                    <div className="content-button flex flex-col md:flex-row justify-between items-baseline py-0">
+                      <p className="lg:px-6 lg:pt-4 px-2 text-md lg:text-lg line-clamp-4 leading-loose lg:w-[1000px]">
+                        {item.paragraph}
+                      </p>
+                      <div className="self-end space-x-8">
+                        <img
+                          className="w-40 h-40  rounded-lg"
+                          src={item.image}
+                          alt="image"
+                        />
+                      </div>
+                    </div>
+                    <div className=" space-x-8 flex flex-col md:flex-row justify-end items-baseline py-0 pr-60">
+                      <Modal article={item} title="post">
+                        <Dialog.Close asChild>
+                          <button
+                            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                            aria-label="Close"
+                          >
+                            <Cross2Icon />
+                          </button>
+                        </Dialog.Close>
+                        <Dialog.Title className=" text-center text-[#1b3476] m-0 text-3xl font-semibold">
+                          Where do you want to post?
+                        </Dialog.Title>
+                        <SocialComponentForPost article={item} />
+                      </Modal>
+                      <Modal article={item} title="schedule">
+                        <Dialog.Close asChild>
+                          <button
+                            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                            aria-label="Close"
+                          >
+                            <Cross2Icon />
+                          </button>
+                        </Dialog.Close>
+                        <Dialog.Title className=" text-center text-[#1b3476] m-0 text-3xl font-semibold">
+                          Where do you want to post?
+                        </Dialog.Title>
+                        <SocialComponentForSchedule article={item} />
+                      </Modal>
                     </div>
                   </div>
-                  <div className=" space-x-8 flex flex-col md:flex-row justify-end items-baseline py-0 pr-60">
-                    <Modal article={item} title="post">
-                      <Dialog.Close asChild>
-                        <button
-                          className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                          aria-label="Close"
-                        >
-                          <Cross2Icon />
-                        </button>
-                      </Dialog.Close>
-                      <Dialog.Title className=" text-center text-[#1b3476] m-0 text-3xl font-semibold">
-                        Where do you want to post?
-                      </Dialog.Title>
-                      <SocialComponentForPost article={item} />
-                    </Modal>
-                    <Modal article={item} title="schedule">
-                      <Dialog.Close asChild>
-                        <button
-                          className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-                          aria-label="Close"
-                        >
-                          <Cross2Icon />
-                        </button>
-                      </Dialog.Close>
-                      <Dialog.Title className=" text-center text-[#1b3476] m-0 text-3xl font-semibold">
-                        Where do you want to post?
-                      </Dialog.Title>
-                      <SocialComponentForSchedule article={item} />
-                    </Modal>
-                  </div>
-                </div>
-              ))}
+                ))}
             </div>
           </div>
         </div>
         <ReactPaginate
           pageCount={pageCount}
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
+          pageRangeDisplayed={pagesToDisplay}
           marginPagesDisplayed={2}
+          onPageChange={handlePageClick}
           previousLabel={<span className="text-black">Previous</span>}
           nextLabel={<span className="text-black">Next</span>}
           containerClassName="flex justify-center items-center my-4 space-x-2"
@@ -132,6 +157,14 @@ const UnscheduledPage = () => {
           breakClassName="p-2"
           activeClassName="bg-customBlue w-[30px] h-[30px] md:w-[40px] md:h-[40px] flex justify-center items-center text-white hover:bg-blue-600 "
         />
+        {showMorePages && (
+          <button
+            className="bg-customBlue text-white p-2 rounded-full cursor-pointer hover:bg-blue-600"
+            onClick={loadMorePages}
+          >
+            &gt;&gt;
+          </button>
+        )}
       </div>
     </div>
   );
