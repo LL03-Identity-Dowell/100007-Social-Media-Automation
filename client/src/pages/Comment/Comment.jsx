f26dcbf33033e3f526bcc11d1179de44b573722e
Victor Yohanna <victoryohanna@gmail.com>
2023-11-15 12:57:13 +0100

    add commemts

--- client/src/pages/Comment/Comment.jsx
+++ client/src/pages/Comment/Comment.jsx
@@ -11,21 +11,27 @@ function Comment({ show }) {
         <div className="text-3xl md:text-4xl text-center">Comments</div>
         <div className="mt-4 p-2 flex flex-row">
           <div className="border rounded-full h-16 w-16 place-content-center text-center"></div>
-          <div className="py-4 pl-1 pr-2 ml-1 w-full">
+          <div className="py-4 pl-1 pr-2 ml-1 w-full ">
             <form>
               <input
                 type="text"
-                className="w-full rounded "
+                className="w-full rounded"
                 placeholder="Type your comments"
               />
             </form>
-            <div id="display-comments" className="border p-2 mt-4 flex flex-row">
+            <div
+              id="display-comments"
+              className=" p-2 mt-4 flex flex-row"
+            >
               <div className="border rounded-full h-12 w-12 "></div>
-              {/* <span className="ml-4 border">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
-                atque ullam a libero aliquid, quos harum quis quasi mollitia,
-                fuga dolor voluptatum, suscipit reprehenderit reiciendis.
-              </span> */}
+              <div className="ml-2 px-2 w-full">
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
+                  quos a eum? Nobis, id beatae. Expedita maiores dolorum porro
+                  ex aut ad perspiciatis adipisci dolorem?
+                </p>
+              </div>
+              {/*  */}
             </div>
           </div>
         </div>
