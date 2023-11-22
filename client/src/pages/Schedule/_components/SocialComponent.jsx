2cc3cee24092d59f9d61ee15bd8977814cb08209
Tomrrohit007 <tomarrohit5034@gmail.com>
2023-11-22 15:54:16 +0530

    social-media-handles-completed

--- client/src/pages/Schedule/_components/SocialComponent.jsx
+++ client/src/pages/Schedule/_components/SocialComponent.jsx
@@ -1,82 +1,153 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import axios from "axios";
 
-export const SocialComponentForPost = ({ article }) => {
-  const onSubmit = (e) => {
+import { useNavigate } from "react-router-dom";
+
+export const SocialComponentForPost = ({
+  article,
+  setError,
+  setLoading,
+  setSuccessMessage,
+  setOpen,
+}) => {
+  const navigate = useNavigate();
+
+  const onSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     const socialArray = Object.fromEntries(new FormData(e.currentTarget));
-    console.log({ ...article, socials: Object.keys(socialArray) });
-  };
-  
-  return (
-    <form className='form' onSubmit={onSubmit}>
-      <div className='flex justify-around'>
-        <label htmlFor='facebook' className='flex flex-row-reverse items-center'>
-          <img src='/facebook.svg' className='w-20 h-20' alt='facebook' />
-          <input
-            name='facebook'
-            className='w-3 h-3'
-            type='checkbox'
-            id='facebook'
-          />
-        </label>
-        <label htmlFor='twitter' className='flex flex-row-reverse items-center'>
-          <img src='/twitter.svg' className='w-20 h-20' alt='twitter' />
-          <input
-            name='twitter'
-            className='w-3 h-3'
-            type='checkbox'
-            id='twitter'
-          />
-        </label>
-        <label htmlFor='instagram' className='flex flex-row-reverse items-center'>
-          <img src='/instagram.svg' className='w-20 h-20' alt='facebook' />
-          <input
-            name='instagram'
-            className='w-3 h-3'
-            type='checkbox'
-            id='instagram'
-          />
-        </label>
-        <label htmlFor='linkedin' className='flex flex-row-reverse items-center'>
-          <img src='/linkedin.svg' className='w-20 h-20' alt='linkedin' />
-          <input
-            name='linkedin'
-            className='w-3 h-3'
-            type='checkbox'
-            id='linkedin'
-          />
-        </label>
+    const mergedData = {
+      ...article,
+      social: Object.keys(socialArray),
+      special: [],
+    };
 
-        <label htmlFor='youtube' className='flex flex-row-reverse items-center'>
-          <img src='/youtube.svg' className='w-20 h-20' alt='youtube' />
-          <input
-            name='youtube'
-            className='w-3 h-3'
-            type='checkbox'
-            id='youtube'
-          />
-        </label>
-        <label htmlFor='pinterest' className='flex flex-row-reverse items-center'>
-          <img src='/pinterest.svg' className='w-20 h-20' alt='pinterest' />
-          <input
-            name='pinterest'
-            className='w-3 h-3'
-            type='checkbox'
-            id='pinterest'
-          />
-        </label>
-      </div>
-      <div className='mt-[25px] flex justify-center '>
-        <button className='px-7 py-2.5 text-base font-medium text-white rounded-md bg-customBlue hover:opacity-95'>
-          Done
-        </button>
-      </div>
-    </form>
+    const url = "http://127.0.0.1:8000/api/v1/media_post/";
+    await axios
+      .post(url, mergedData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setError(null);
+        setLoading(false);
+        setSuccessMessage("Successfully submit for post");
+        console.log(res);
+
+        setTimeout(() => {
+          navigate("/recent");
+        }, 1000);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError("Server error, Please try again later");
+        console.error("Error fetching article:", error);
+        setSuccessMessage(null);
+      });
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='flex justify-around'>
+          <label
+            htmlFor='facebook'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/facebook.svg' className='w-20 h-20' alt='facebook' />
+            <input
+              name='facebook'
+              className='w-3 h-3'
+              type='checkbox'
+              id='facebook'
+            />
+          </label>
+          <label
+            htmlFor='twitter'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/twitter.svg' className='w-20 h-20' alt='twitter' />
+            <input
+              name='twitter'
+              className='w-3 h-3'
+              type='checkbox'
+              id='twitter'
+            />
+          </label>
+          <label
+            htmlFor='instagram'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/instagram.svg' className='w-20 h-20' alt='facebook' />
+            <input
+              name='instagram'
+              className='w-3 h-3'
+              type='checkbox'
+              id='instagram'
+            />
+          </label>
+          <label
+            htmlFor='linkedin'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/linkedin.svg' className='w-20 h-20' alt='linkedin' />
+            <input
+              name='linkedin'
+              className='w-3 h-3'
+              type='checkbox'
+              id='linkedin'
+            />
+          </label>
+
+          <label
+            htmlFor='youtube'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/youtube.svg' className='w-20 h-20' alt='youtube' />
+            <input
+              name='youtube'
+              className='w-3 h-3'
+              type='checkbox'
+              id='youtube'
+            />
+          </label>
+          <label
+            htmlFor='pinterest'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/pinterest.svg' className='w-20 h-20' alt='pinterest' />
+            <input
+              name='pinterest'
+              className='w-3 h-3'
+              type='checkbox'
+              id='pinterest'
+            />
+          </label>
+        </div>
+        <div className='mt-[25px] flex justify-center '>
+          <button
+            type='submit'
+            className='px-7 py-2.5 text-base font-medium text-white rounded-md bg-customBlue hover:opacity-95'
+          >
+            Done
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
-export const SocialComponentForSchedule = ({ article }) => {
-  const onSubmit = (e) => {
+export const SocialComponentForSchedule = ({
+  article,
+  setError,
+  setLoading,
+  setSuccessMessage,
+  setOpen,
+}) => {
+  const navigate = useNavigate();
+  const onSubmit = async (e) => {
+    setLoading(true);
+
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const socialArray = Array.from(formData.entries())
@@ -88,85 +159,132 @@ export const SocialComponentForSchedule = ({ article }) => {
     const mergedData = {
       ...article,
       time: datetimeInput,
-      socials: socialArray,
+      social: socialArray,
+      special: [],
+      schedule: "11/23/2023 21:27:00",
     };
 
-    console.log(mergedData);
+    const url = "http://127.0.0.1:8000/api/v1/media_schedule/";
+
+    await axios
+      .post(url, mergedData, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setError(null);
+        setLoading(false);
+        setSuccessMessage("Successfully submit for schedule");
+        console.log(res);
+        setTimeout(() => {
+          navigate("/unscheduled");
+        }, 1000);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError("Server error, Please try again later");
+        console.error("Error fetching article:", error);
+        setSuccessMessage(null);
+      });
+    setOpen(false);
   };
+
   return (
-    <form className='form' onSubmit={onSubmit}>
-      <div className='flex justify-around'>
-        <label htmlFor='facebook' className='flex flex-row-reverse items-center'>
-          <img src='/facebook.svg' className='w-20 h-20' alt='facebook' />
-          <input
-            name='facebook'
-            className='w-3 h-3'
-            type='checkbox'
-            id='facebook'
-          />
-        </label>
-        <label htmlFor='twitter' className='flex flex-row-reverse items-center'>
-          <img src='/twitter.svg' className='w-20 h-20' alt='twitter' />
-          <input
-            name='twitter'
-            className='w-3 h-3'
-            type='checkbox'
-            id='twitter'
-          />
-        </label>
-        <label htmlFor='instagram' className='flex flex-row-reverse items-center'>
-          <img src='/instagram.svg' className='w-20 h-20' alt='facebook' />
-          <input
-            name='instagram'
-            className='w-3 h-3'
-            type='checkbox'
-            id='instagram'
-          />
-        </label>
-        <label htmlFor='linkedin' className='flex flex-row-reverse items-center'>
-          <img src='/linkedin.svg' className='w-20 h-20' alt='linkedin' />
-          <input
-            name='linkedin'
-            className='w-3 h-3'
-            type='checkbox'
-            id='linkedin'
-          />
-        </label>
-
-        <label htmlFor='youtube' className='flex flex-row-reverse items-center'>
-          <img src='/youtube.svg' className='w-20 h-20' alt='youtube' />
-          <input
-            name='youtube'
-            className='w-3 h-3'
-            type='checkbox'
-            id='youtube'
-          />
-        </label>
-        <label htmlFor='pinterest' className='flex flex-row-reverse items-center'>
-          <img src='/pinterest.svg' className='w-20 h-20' alt='pinterest' />
-          <input
-            name='pinterest'
-            className='w-3 h-3'
-            type='checkbox'
-            id='pinterest'
-          />
-        </label>
-      </div>
-      <div className='mt-[25px] flex justify-center space-x-6'>
-        <input type='datetime-local' name='datetime' />
-        <Dialog.Close asChild>
-          <button
-            type='button'
-            className='w-24 h-10 text-sm text-white rounded-md hover:opacity-95 bg-[#5c6388]'
+    <>
+      <form className='form' onSubmit={onSubmit}>
+        <div className='flex justify-around'>
+          <label
+            htmlFor='facebook'
+            className='flex flex-row-reverse items-center'
           >
-            Remove
-          </button>
-        </Dialog.Close>
+            <img src='/facebook.svg' className='w-20 h-20' alt='facebook' />
+            <input
+              name='facebook'
+              className='w-3 h-3'
+              type='checkbox'
+              id='facebook'
+            />
+          </label>
+          <label
+            htmlFor='twitter'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/twitter.svg' className='w-20 h-20' alt='twitter' />
+            <input
+              name='twitter'
+              className='w-3 h-3'
+              type='checkbox'
+              id='twitter'
+            />
+          </label>
+          <label
+            htmlFor='instagram'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/instagram.svg' className='w-20 h-20' alt='facebook' />
+            <input
+              name='instagram'
+              className='w-3 h-3'
+              type='checkbox'
+              id='instagram'
+            />
+          </label>
+          <label
+            htmlFor='linkedin'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/linkedin.svg' className='w-20 h-20' alt='linkedin' />
+            <input
+              name='linkedin'
+              className='w-3 h-3'
+              type='checkbox'
+              id='linkedin'
+            />
+          </label>
 
-        <button className='px-7 py-2.5 text-base font-medium text-white rounded-md bg-customBlue hover:opacity-95'>
-          Done
-        </button>
-      </div>
-    </form>
+          <label
+            htmlFor='youtube'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/youtube.svg' className='w-20 h-20' alt='youtube' />
+            <input
+              name='youtube'
+              className='w-3 h-3'
+              type='checkbox'
+              id='youtube'
+            />
+          </label>
+          <label
+            htmlFor='pinterest'
+            className='flex flex-row-reverse items-center'
+          >
+            <img src='/pinterest.svg' className='w-20 h-20' alt='pinterest' />
+            <input
+              name='pinterest'
+              className='w-3 h-3'
+              type='checkbox'
+              id='pinterest'
+            />
+          </label>
+        </div>
+        <div className='mt-[25px] flex justify-center space-x-6'>
+          <input type='datetime-local' name='datetime' />
+          <Dialog.Close asChild>
+            <button
+              type='button'
+              className='w-24 h-10 text-sm text-white rounded-md hover:opacity-95 bg-[#5c6388]'
+            >
+              Remove
+            </button>
+          </Dialog.Close>
+
+          <button
+            type='submit'
+            className='px-7 py-2.5 text-base font-medium text-white rounded-md bg-customBlue hover:opacity-95'
+          >
+            Done
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
