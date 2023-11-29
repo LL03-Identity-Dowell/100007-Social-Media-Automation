a149e3c1df752d52563f1c97488447f144d4cef3
Victor Yohanna <victoryohanna@gmail.com>
2023-11-29 03:16:59 +0100

    creates comments component

--- client/src/pages/Comment/Comment.jsx
+++ client/src/pages/Comment/Comment.jsx
@@ -1,35 +1,37 @@
 import React, { useEffect } from "react";
+import ExtraSmallBtn from "../../components/ExtraSmallBtn/ExtraSmallBtn";
+
+import { DummyData } from "./Data";
 
 function Comment({ show }) {
+  
   useEffect(() => {
     show();
+    
   }, []);
 
   return (
     <div className="relative h-[100vh] max-w-7xl mx-auto lg:h-auto overflow-y-hidden lg:overflow-y-auto">
-      <div className="w-3/5 m-auto p-4 border">
-        <div className="text-3xl md:text-4xl text-center">Comments</div>
-        <div className="mt-4 p-2 flex flex-row">
-          <div className="border rounded-full h-16 w-16 place-content-center text-center"></div>
-          <div className="py-4 pl-1 pr-2 ml-1 w-full ">
-            <form>
-              <input
-                type="text"
-                className="w-full rounded"
-                placeholder="Type your comments"
-              />
-            </form>
-            <div id="display-comments" className=" p-2 mt-4 flex flex-row">
-              <div className="border rounded-full h-12 w-12 "></div>
-              <div className="ml-2 px-2 w-full">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
-                  quos a eum? Nobis, id beatae. Expedita maiores dolorum porro
-                  ex aut ad perspiciatis adipisci dolorem?
+      <div className="w-full text-sm text-left text-gray-500 dark:text-gray-400 ">
+        <div className="text-center text-customTextBlue font-semibold py-2 lg:py-6">
+          <h2 className="text-3xl md:text-4xl ">Comments</h2>
+        </div>
+        <div className="comments">
+          {DummyData.map((item) => (
+            <div className="w-[90%] m-auto " key={item.id}>
+              <p className="lg:px-6 px-2 py-0 text-md lg:text-xl text-customTextBlue dark:text-white font-bold ">
+                {item.title}
+              </p>
+              <div className="flex flex-col md:flex-row justify-between items-baseline py-0">
+                <p className="lg:px-6 lg:pt-4 px-2 text-md lg:text-lg w-full line-clamp-4 leading-loose">
+                  {item.text}
                 </p>
               </div>
+              <div className=" lg:pt-2 flex justify-end md:mr-6 mt-2 md:mt-0 ">
+                <ExtraSmallBtn title={"View Comments"} />
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
