31f344e4168e9cfc320c0973275d7c1d5fdcd664
James Okolie <zebedee788@gmail.com>
2023-11-29 10:14:19 +0200

    Link social media function

--- client/src/pages/UserProfile/SocialMediaChannels.jsx
+++ client/src/pages/UserProfile/SocialMediaChannels.jsx
@@ -5,39 +5,76 @@ import {
   FaPinterest,
   FaTwitter,
   FaYoutube,
+  FaArrowRight 
 } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import UserWrapper from "./UserWrapper";
 import axios from "axios";
 
-const SocialMediaChannels = () => {
+import Loading from "../../components/Loading";
+import { ErrorMessages, SuccessMessages } from "../../components/Messages";
 
-axios.get(`http://127.0.0.1:8000/api/v1/social_media_channels/`, {
+
+
+const SocialMediaChannels = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [activateLink, setActivateLink] = useState({
+    activate: "Activate",
+    info: "Click to activate"
+  })
+
+  useEffect(() => {
+    checkUserSocialLinking();
+    
+  }, [])
+
+ 
+
+
+  const checkUserSocialLinking = () => {
+    
+    // setLoading(true)
+    axios.get(`http://127.0.0.1:8000/api/v1/link/linkusers/`, {
           withCredentials: true,
         })
         .then((response) => {
           // setLoading(false)
           let resData = response.data;
           console.log(resData)
-          // setSuccess("Topics created successfully!");
-          // setTimeout(() => {
-          //   handleSentenceNavigate(resData)
-          // }, 2000);
         })
         .catch((error) => {
           // setLoading(false);
-          // setError("Error creating topics..!");
-          console.error("Error fetching Social Media:", error);
+          if(error.response.status == 400 && error.response.statusText == "Bad Request" && error.response.data.includes("Profile title already exists")){
+            setActivateLink({
+              activate: "Link Social Media Accounts",
+              info: "Clck to Link"
+            })
+            // setError(error.response.data);
+          }
+          // console.error("Error fetching Social Media:", error);
+          console.log(error.response.data);
         });
 
       // setTimeout(() => {
       //   setError("");
       // }, 4000)
 
+  }
+ 
+  
+
+  
+
 
   return (
     <UserWrapper>
       <div className='flex flex-col items-center justify-center w-full h-full'>
+        {loading && <Loading />}
+        {error && <ErrorMessages>{error}</ErrorMessages>}
+        {success && <SuccessMessages>{success}</SuccessMessages>}
         <div>
           <h2 className='pb-12 text-2xl font-bold text-customBlue xl:text-4xl'>
             My Channels
@@ -64,14 +101,61 @@ axios.get(`http://127.0.0.1:8000/api/v1/social_media_channels/`, {
           </Link>
         </div>
         <div className='flex items-center justify-center mt-6 md:mt-8'>
-          <Link
-            to='/'
+          <Link data-modal-target="select-modal" data-modal-toggle="select-modal"
+            to=""
             className='flex items-center gap-3 px-10 py-2 text-white rounded-md bg-customBlue'
           >
             Connect
           </Link>
         </div>
       </div>
+
+
+      {/* Modal */}
+      <div id="select-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div className="relative p-4 w-full max-w-md max-h-full">
+        {/* Modal content */}
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          {/* Modal header */}
+          <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-lg font-semibold text-customTextBlue dark:text-white">
+              Social Media Accounts Linking
+            </h3>
+            <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="select-modal">
+              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+          </div>
+          {/* Modal body */}
+          <div className="p-4 md:p-5">
+            <ul className="space-y-4 mb-4">
+              <li>
+                <Link to="http://127.0.0.1:8000/api/v1/link/" target="_blank">
+                <input type="button" id="social" className="hidden peer"  />
+                <label htmlFor="job-1" className="inline-flex items-center justify-between w-full p-5 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
+                  <div className="block">
+                    <div className="w-full text-lg font-semibold">{activateLink.activate}</div>
+                    <div className="w-full text-gray-500 dark:text-gray-400">{activateLink.info}</div>
+                  </div>
+                  <FaArrowRight  className="w-4 h-4 ms-3 rtl:rotate-180 text-gray-500 dark:text-gray-400" />
+                </label>
+                </Link>
+              </li>
+              {/* Add similar code for other job options */}
+            </ul>
+            <Link data-modal-toggle="select-modal" to="/user-profile">
+                <button className="text-white inline-flex w-full justify-center bg-customBlue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              Done
+            </button>
+            </Link>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
     </UserWrapper>
   );
 };
