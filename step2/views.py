49439c4df3487a2014434ae92d76ddf3891af281
wilfex <wilfex81@gmail.com>
2023-11-29 11:51:30 +0300

    fix: session_id issue

--- step2/views.py
+++ step2/views.py
@@ -168,15 +168,16 @@ def register(request):
 @method_decorator(csrf_exempt, name='dispatch')
 class MainAPIView(APIView):
     def get(self, request):
-        session_id = request.session.get(
-            "session_id") or request.GET.get('session_id')
-        if not session_id:
-            session_id = request.META.get('HTTP_AUTHORIZATION').split(' ')[:-1]
-        if session_id:
+        # session_id = request.session.get(
+        #     "session_id") or request.GET.get('session_id')
+        # if not session_id:
+        #     session_id = request.META.get('HTTP_AUTHORIZATION').split(' ')[:-1]
+        # if session_id:
+        if request.session.get("session_id"):
             user_map = {}
             redirect_to_living_lab = True
             url_1 = "https://100093.pythonanywhere.com/api/userinfo/"
-
+            session_id = request.session["session_id"]
             response_1 = requests.post(url_1, data={"session_id": session_id})
             if response_1.status_code == 200 and "portfolio_info" in response_1.json():
                 profile_details = response_1.json()
