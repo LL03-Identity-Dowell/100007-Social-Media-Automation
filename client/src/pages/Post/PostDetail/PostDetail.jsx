4adaccafbae0b0c78035b1d728e1fe32c5bc9944
Noble <nobleuwuja@gmail.com>
2024-01-11 15:35:02 +0100

    iframe in the post details page

--- client/src/pages/Post/PostDetail/PostDetail.jsx
+++ client/src/pages/Post/PostDetail/PostDetail.jsx
@@ -16,9 +16,12 @@ import Dropdown from "./_components/dropdown";
 import Counts from "./_components/counts";
 import CarosuelModal from "./_components/carosuel-modal";
 import { handleCharacCount } from "./_components/function";
+import { FaEdit, FaTimes } from "react-icons/fa";
 
 function PostDetail({ show }) {
   const [editing, setEditing] = useState(false);
+  const [postId, setPostId] = useState(null);
+  const [iframeSrc, setIframeSrc] = useState(null);
   const [postDetailData, setPostDetailData] = useState();
   const [designedFor, setDesignedFor] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -35,6 +38,11 @@ function PostDetail({ show }) {
     designed_for: "Twitter-uxlivinglab",
     targeted_category: "Brand",
   });
+
+  // const iframeSrc =
+  //   "https://ll04-finance-dowell.github.io/100058-DowellEditor-V2/?token=eyJhbGciOiJIUzI1NiJ9.eyJwcm9kdWN0X25hbWUiOiJTb2NpYWwgTWVkaWEgQXV0b21hdGlvbiIsImRldGFpbHMiOnsiX2lkIjoiNjU3ODVmNWIzNWRhZDQzMjZlMDRiMGRjIiwiZGF0YWJhc2UiOiJzb2NpYWxtZWRpYSIsImNvbGxlY3Rpb24iOiJzdGVwM19kYXRhIiwidGVhbV9tZW1iZXJfSUQiOiIzNDU2Nzg5Nzc5OSIsImZ1bmN0aW9uX0lEIjoiQUJDREUiLCJjbHVzdGVyIjoic29jaWFsbWVkaWEiLCJkb2N1bWVudCI6InN0ZXAzX2RhdGEiLCJjb21tYW5kIjoiZmV0Y2giLCJ1cGRhdGVfZmllbGQiOnsib3JkZXJfbm9zIjoyMX19fQ.QTlwdOv7y1E6YcAe7hjyVLF9k2vNx_vdheoshLS3sOU";
+
+ 
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -128,6 +136,9 @@ function PostDetail({ show }) {
           setDesignedFor(data.linked_accounts);
           // console.log(data);
           setPostDetailData(data);
+          
+          let isPostId = data.post._id;
+          setPostId(isPostId)
           let paragraph = data.post.paragraph[0];
           // console.log(paragraph)
           paragraph = paragraph.split("\n\n");
@@ -149,6 +160,29 @@ function PostDetail({ show }) {
     }
   };
 
+  const handelPopup = ()=>{
+    axios
+    .get(`http://127.0.0.1:8000/api/v1/edit_post/${postId}/`, {
+      withCredentials: true,
+    })
+    .then((response) => {
+      setError(null);
+      setLoading(false);
+      console.log(response);
+      let resData = response.data.redirect_url;
+      setIframeSrc(resData)
+
+      
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError("Please try again later");
+      console.error("Error Editing post:", error);
+    });
+
+    setEditing(!editing)
+  }
+
   const handleNavigation = () => {
     if (!editing) {
       navigate("/scheduled");
@@ -158,125 +192,113 @@ function PostDetail({ show }) {
   };
 
   return (
-    <div className='m-4 mb-8 lg:m-8'>
+    <div className="m-4 mb-8 lg:m-8">
       {loading && <Loading />}
       {error && <ErrorMessages>{error}</ErrorMessages>}
       {success && <SuccessMessages>{success}</SuccessMessages>}
-      <div className='flex flex-row-reverse'>
+      {/* <div className="flex flex-row-reverse">
         <Dropdown
-          editPost={() => setEditing(true)}
+          editPost={handelPopup}
           savePost={() => setEditing(false)}
           handleSubmit={handleSubmit}
         />
-      </div>
+      </div> */}
 
-      <div className='mt-2 mb-2 text-lg font-bold md:mb-0'>
-        {!editing ? (
+      <div className="mt-2 mb-2 text-lg font-bold md:mb-0 flex justify-between items-center">
+       
           <h2>
-            {/* {postDetailData && postDetailData.post.title} */}
-            The team was adviced by the cyber security experts.
+            {postDetailData && postDetailData.post.title}
+            {/* The team was adviced by the cyber security experts. */}
           </h2>
-        ) : (
-          <input
-            className='w-full px-1 py-2 border border-[#6B7280] outline-[#3f83f8]'
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        )}
+        
+        <span onClick={handelPopup} className="cursor-pointer text-[15px] flex gap-2 items-center bg-gray-400 hover:bg-customTextBlue text-white  py-1 px-4 rounded-lg"> <FaEdit/>Edit</span>
       </div>
 
-      <hr className='my-4' />
+      <hr className="my-4" />
 
-      <div className='mt-4 md:mt-8'>
-        {!editing ? (
+      <div className="mt-4 md:mt-8">
+        {
           postDetailData &&
           paragraph.map((p, index) => (
-            <div className='text-base' key={index}>
+            <div className="text-base" key={index}>
               {index >= 0 && p}
             </div>
           ))
-        ) : (
-          <textarea
-            className='w-full px-1 py-2 outline-none ring-0 border border-[#6B7280]'
-            rows={6}
-            value={paragraph}
-            onChange={(e) => setParagraph(e.target.value)}
-          />
-        )}
+         }
       </div>
 
-      <hr className='my-4' />
+      <hr className="my-4" />
 
-      <div id='post-sources' className='mt-4 md:mt-8'>
+      <div id="post-sources" className="mt-4 md:mt-8">
         <Link
-          to='https://openai.com'
-          className='text-blue-500 post-source hover:underline'
+          to="https://openai.com"
+          className="text-blue-500 post-source hover:underline"
         >
           {postDetailData && postDetailData.post.source}
         </Link>
       </div>
 
-      <hr className='my-4' />
+      <hr className="my-4" />
 
-      <div className='flex flex-col lg:flex-row md:flex-row md:gap-20 lg:gap-24'>
-        <div className='relative'>
+      <div className="flex flex-col lg:flex-row md:flex-row md:gap-20 lg:gap-24">
+        <div className="relative">
           <img
             src={postDetailData && postDetailData.images}
-            alt='Random image'
-            className='w-full h-full img-fluid post-img'
+            alt="Random image"
+            className="w-full h-full img-fluid post-img"
           />
           {editing && <CarosuelModal setSelectedImage={setSelectedImage} />}
         </div>
 
-        <div className='flex flex-col gap-6 mt-5 post-options'>
-          <div className='flex flex-col lg:flex-row lg:gap-8'>
+        <div className="flex flex-col gap-6 mt-5 post-options">
+          <div className="flex flex-col lg:flex-row lg:gap-8">
             <label
-              htmlFor='content'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              htmlFor="content"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               <strong>Qualitative categorization:</strong>
             </label>
             <select
               value={inputs.qualitative_categorization}
-              name='qualitative_categorization'
+              name="qualitative_categorization"
               onChange={handelChange}
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <QualitativeCategorizationList />
             </select>
           </div>
 
-          <div className='flex flex-col lg:flex-row lg:gap-8'>
+          <div className="flex flex-col lg:flex-row lg:gap-8">
             <label
-              htmlFor='brand'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              htmlFor="brand"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               <strong>Targeted for:</strong>
             </label>
             <select
               value={inputs.targeted_for}
-              id='brand'
+              id="brand"
               onChange={handelChange}
-              name='targeted_for'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              name="targeted_for"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <TargetForListOptions />
             </select>
           </div>
 
-          <div className='flex flex-col lg:flex-row lg:gap-8'>
+          <div className="flex flex-col lg:flex-row lg:gap-8">
             <label
-              htmlFor='channel'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              htmlFor="channel"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               <strong>Designed for:</strong>
             </label>
             <select
               value={inputs.designed_for}
-              id='channel'
+              id="channel"
               onChange={handelChange}
-              name='designed_for'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              name="designed_for"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               {designedFor &&
                 designedFor.map((socialMedia, index) => (
@@ -287,19 +309,19 @@ function PostDetail({ show }) {
             </select>
           </div>
 
-          <div className='flex flex-col lg:flex-row lg:gap-8'>
+          <div className="flex flex-col lg:flex-row lg:gap-8">
             <label
-              htmlFor='countries'
-              className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+              htmlFor="countries"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
             >
               <strong>Targeted category:</strong>
             </label>
             <select
-              id='channelbrand'
+              id="channelbrand"
               value={inputs.targeted_category}
               onChange={handelChange}
-              name='targeted_category'
-              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              name="targeted_category"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
               <TargetCategory />
             </select>
@@ -307,7 +329,7 @@ function PostDetail({ show }) {
         </div>
       </div>
 
-      <hr className='my-4' />
+      <hr className="my-4" />
 
       <Counts
         characCount={characterCount}
@@ -315,17 +337,27 @@ function PostDetail({ show }) {
         wordCount={wordCount}
       />
 
-      <div className='flex justify-center gap-20 mt-8'>
+      <div className="flex justify-center gap-20 mt-8">
         <UnstyledButton
-          text='Back'
-          className='text-base font-semibold bg-customBlue w-[128px] hover:bg-blue- text-center'
+          text="Back"
+          className="text-base font-semibold bg-customBlue w-[128px] hover:bg-blue- text-center"
         />
         <UnstyledButton
-          text='Next'
-          className='text-base font-semibold bg-customBlue w-[128px] hover:bg-blue- text-center'
+          text="Next"
+          className="text-base font-semibold bg-customBlue w-[128px] hover:bg-blue- text-center"
           onClick={handleNavigation}
         />
       </div>
+
+      {/* Editing Modal Starts */}
+      <div className={editing ? "fixed z-40 top-1/2 right-1/2 translate-x-1/2 -translate-y-1/2 transformrounded-xl w-full flex justify-center items-center h-full bg-overlay" : "hidden"}>
+        <div className=" bg-white w-full h-full 2xl:h-[900px] md:w-[900px] md:h-[600px]  text-black relative">
+        <span className="font-bold text-xl p-2 border border-black rounded-full absolute -right-12 cursor-pointer top-0" onClick={handelPopup}><FaTimes/></span>
+        <iframe src={iframeSrc} width="100%" height="100%"/>
+
+        </div>
+      </div>
+      {/* Editing Modal Ends */}
     </div>
   );
 }
