34a68e4bf51cc22df511f0b830ce762b28805931
Abel <abelmengistu20@gmail.com>
2024-01-23 23:46:15 +0300

    added all  view model from website app to the url off automate directory

--- automate/urls.py
+++ automate/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import RankAutomationAPIView
+from . import views
 
 urlpatterns = [
     path('rank-automation/', RankAutomationAPIView.as_view(), name='rank_automation'),
