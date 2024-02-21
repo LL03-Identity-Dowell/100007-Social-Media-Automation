bfaed957ce4059602206d91969be9542f009bfde
enockWafula <enocksimiyu8@gmail.com>
2024-02-21 14:54:57 +0300

    Updated the credits handler to be used by automation code

--- credits/credit_handler.py
+++ credits/credit_handler.py
@@ -2,21 +2,24 @@ from django.contrib import messages
 from django.core.handlers.wsgi import WSGIRequest
 
 from credits.constants import STEP_1_SUB_SERVICE_ID, STEP_2_SUB_SERVICE_ID, STEP_3_SUB_SERVICE_ID, \
-    STEP_4_SUB_SERVICE_ID, SERVICE_ID,COMMENTS_SUB_SERVICE_ID
+    STEP_4_SUB_SERVICE_ID, SERVICE_ID, COMMENTS_SUB_SERVICE_ID
 from credits.credit_manager import Credit
 from credits.exceptions import CouldNotConsumeCreditError, CouldNotGetUserAPIKeyError
 
 
 class CreditHandler:
 
-    def format_response(self, request: WSGIRequest, response: dict):
+    def format_response(self, request: WSGIRequest = None, response=None):
+        if response is None:
+            response = {}
         data = response
         credit_response = {}
         if not data:
             credit_response.update(
                 {'success': False, 'message': 'Please create a service/api Key for this workspace', 'error_code': 1})
             # PLEASE CREATE SERVICE/API KEY FOR THIS WORKSPACE
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
         is_active = data.get("is_active")
         user_credits = data.get("total_credits")
@@ -26,21 +29,24 @@ class CreditHandler:
             credit_response.update(
                 {'success': False, 'message': 'Please activate the service/api key', 'error_code': 2})
             # PLEASE ACTIVATE THE SERVICE/API KEY
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         if disable_key:
             credit_response.update(
                 {'success': False, 'message': 'Your service/api key is disabled', 'error_code': 3})
             # YOUR SERVICE/API KEY IS DISABLED
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         if user_credits <= 0:
             credit_response.update(
                 {'success': False, 'message': "kindly buy credits", "link": "https://uxlivinglab.com/",
                  'error_code': 4})
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         # Checking if social media service is active or not
@@ -53,24 +59,28 @@ class CreditHandler:
         if not social_media_service:
             credit_response.update(
                 {'success': False, 'message': 'Social Media service not found'})
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         if not social_media_service.get('is_active'):
             credit_response.update(
                 {'success': False, 'message': 'Please activate social media service'})
             # PLEASE ACTIVATE THE SOCIAL MEDIA SERVICE
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
-
-        request.session['remaining_credits'] = response.get(
-            'remaining_credits')
-        if request.session.get('credit_response', ):
-            del request.session['credit_response']
+        if request:
+            request.session['remaining_credits'] = response.get(
+                'remaining_credits')
+            if request.session.get('credit_response', ):
+                del request.session['credit_response']
         response['success'] = True
         return response
 
-    def format_steps_response(self, request: WSGIRequest, response: dict):
+    def format_steps_response(self, request: WSGIRequest = None, response=None):
+        if response is None:
+            response = {}
         data = response
 
         if not data:
@@ -78,14 +88,18 @@ class CreditHandler:
         if not data.get('success'):
             messages.error(request, response.get('message'))
             return response
-        request.session['remaining_credits'] = response.get(
-            'remaining_credits')
-        messages.success(request, 'Credits was successfully consumed')
+        if request:
+            request.session['remaining_credits'] = response.get(
+                'remaining_credits')
+            messages.success(request, 'Credits was successfully consumed')
         return response
 
-    def login(self, request: WSGIRequest):
+    def login(self, request: WSGIRequest = None, user_info=None):
+        if user_info is None:
+            user_info = {}
         credit = Credit()
-        user_info = request.session.get('userinfo', None)
+        if request:
+            user_info = request.session.get('userinfo', None)
 
         if not user_info:
             response = {}
@@ -98,18 +112,23 @@ class CreditHandler:
             response = credit.get_user_api_key(work_space_id=client_admin_id)
             data = response.get('data')
             api_key = data.get('api_key')
-            request.session['CREDIT_API_KEY'] = api_key
+            if request:
+                request.session['CREDIT_API_KEY'] = api_key
             response = data
         except CouldNotGetUserAPIKeyError:
             response = {}
         return self.format_response(request, response)
 
-    def consume_step_1_credit(self, request: WSGIRequest):
+    def consume_step_1_credit(self, request: WSGIRequest = None, user_info=None):
         """
         This method consumes credits on step 1
         """
-
-        user_api_key = request.session.get('CREDIT_API_KEY')
+        user_api_key = ''
+        if request:
+            user_api_key = request.session.get('CREDIT_API_KEY')
+        elif user_info:
+            response = self.login(request=request, user_info=user_info)
+            user_api_key = response.get('api_key')
         if not user_api_key:
             return {'success': False, 'message': 'You do not have service id'}
         credit = Credit(user_api_key)
@@ -123,11 +142,16 @@ class CreditHandler:
             response = {}
         return self.format_steps_response(request, response)
 
-    def consume_step_2_credit(self, request: WSGIRequest):
+    def consume_step_2_credit(self, request: WSGIRequest = None, user_info=None):
         """
         This method consumes credits on step 1
         """
-        user_api_key = request.session.get('CREDIT_API_KEY')
+        user_api_key = ''
+        if request:
+            user_api_key = request.session.get('CREDIT_API_KEY')
+        elif user_info:
+            response = self.login(request=request, user_info=user_info)
+            user_api_key = response.get('api_key')
         if not user_api_key:
             return {'success': False, 'message': 'You do not have service id'}
         credit = Credit(user_api_key)
@@ -141,11 +165,16 @@ class CreditHandler:
             response = {}
         return self.format_steps_response(request, response)
 
-    def consume_step_3_credit(self, request: WSGIRequest):
+    def consume_step_3_credit(self, request: WSGIRequest = None, user_info=None):
         """
         This method consumes credits on step 3
         """
-        user_api_key = request.session.get('CREDIT_API_KEY')
+        user_api_key = ''
+        if request:
+            user_api_key = request.session.get('CREDIT_API_KEY')
+        elif user_info:
+            response = self.login(request=request, user_info=user_info)
+            user_api_key = response.get('api_key')
         if not user_api_key:
             return {'success': False, 'message': 'You do not have service id'}
         credit = Credit(user_api_key)
@@ -159,11 +188,16 @@ class CreditHandler:
             response = {}
         return self.format_steps_response(request, response)
 
-    def consume_step_4_credit(self, request: WSGIRequest):
+    def consume_step_4_credit(self, request: WSGIRequest = None, user_info=None):
         """
         This method consumes credits on step 4
         """
-        user_api_key = request.session.get('CREDIT_API_KEY')
+        user_api_key = ''
+        if request:
+            user_api_key = request.session.get('CREDIT_API_KEY')
+        elif user_info:
+            response = self.login(request=request, user_info=user_info)
+            user_api_key = response.get('api_key')
         if not user_api_key:
             return {'success': False, 'message': 'You do not have service id'}
         credit = Credit(user_api_key)
@@ -177,11 +211,16 @@ class CreditHandler:
             response = {}
         return self.format_steps_response(request, response)
 
-    def consume_step_5_credit(self, request: WSGIRequest):
+    def consume_step_5_credit(self, request: WSGIRequest = None, user_info=None):
         """
         This method consumes credits on step 5
         """
-        user_api_key = request.session.get('CREDIT_API_KEY')
+        user_api_key = ''
+        if request:
+            user_api_key = request.session.get('CREDIT_API_KEY')
+        elif user_info:
+            response = self.login(request=request, user_info=user_info)
+            user_api_key = response.get('api_key')
         if not user_api_key:
             return {'success': False, 'message': 'You do not have service id'}
         credit = Credit(user_api_key)
@@ -195,17 +234,18 @@ class CreditHandler:
             response = {}
         return self.format_steps_response(request, response)
 
-    def check_if_user_has_enough_credits(self, sub_service_id: str, request: WSGIRequest):
+    def check_if_user_has_enough_credits(self, sub_service_id: str, request: WSGIRequest = None, user_info=None):
         """
         This method check if a user has enough credits to perform a step
         """
         credit_response = {}
-        response = self.login(request)
+        response = self.login(request=request, user_info=user_info)
 
         if not response.get('services'):
             credit_response.update(
                 {'success': False, 'message': 'Please activate the social media service'})
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         services = response.get('services')
@@ -217,7 +257,8 @@ class CreditHandler:
         if not social_media_service:
             credit_response.update(
                 {'success': False, 'message': 'Social Media service not found'})
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
 
         sub_service_data: dict = {}
@@ -226,10 +267,12 @@ class CreditHandler:
                 sub_service_data = sub_service
                 break
         if not sub_service_data:
-            messages.error(request, 'Please activate the service/api key')
+
             credit_response.update(
                 {'success': False, 'message': 'Please activate the service/api key'})
-            request.session['credit_response'] = credit_response
+            if request:
+                messages.error(request, 'Please activate the service/api key')
+                request.session['credit_response'] = credit_response
             return credit_response
 
         sub_service_credits = sub_service_data.get('sub_service_credits')
@@ -239,6 +282,7 @@ class CreditHandler:
             message = f'You do not have enough credits to access and/or perform actions on this step. You have {str(remaining_credits)} credits. Required credits are: {str(sub_service_credits)}'
 
             credit_response.update({'success': False, 'message': message})
-            request.session['credit_response'] = credit_response
+            if request:
+                request.session['credit_response'] = credit_response
             return credit_response
         return {'success': True, 'message': 'You have enough credits to perform this action'}
