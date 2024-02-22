8737e1461f77baef0658afc7afc302d3211fdfa6
enockWafula <enocksimiyu8@gmail.com>
2024-02-22 17:57:14 +0300

    Added environment variable for accepting social media requests

--- step2/views.py
+++ step2/views.py
@@ -1280,7 +1280,7 @@ class AdminApproveSocialMediaRequestView(AuthenticatedBaseView):
 
     def post(self, request, *args, **kwargs):
         username = request.session.get('username')
-        if username is not SOCIAL_MEDIA_ADMIN_APPROVE_USERNAME:
+        if username != SOCIAL_MEDIA_ADMIN_APPROVE_USERNAME:
             return Response({'message': 'You are not authorized to access this page'}, status=HTTP_401_UNAUTHORIZED)
         step_2_manager = Step2Manager()
         serializer = SocialMediaRequestSerializer(data=request.data)
